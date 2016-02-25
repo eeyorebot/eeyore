233a64bc86d3e33a10d153edb10f136294df7144
Steve Klabnik <steve@steveklabnik.com>
2016-02-25 16:59:32 -0500

    initial hubcaps example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate hyper;
+extern crate hubcaps;
+
+use hyper::Client;
+use hubcaps::Github;
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::new();
+    let github = Github::new(
+        "my-cool-user-agent/0.1.0",
+        &client,
+        None::<String>,
+        );
+
+    let repo = github.repo("rust-lang", "rust");
+
+    let labels = repo.labels();
+
+    for l in labels.list().unwrap() {
+        println!("{:?}", l)
+    }
 }
